99a51d1695a9eb10c3a87b770fbae64efab9ec32
Pedro Garcia <pedrofsgarcia.pro@gmail.com>
2026-03-30 10:57:07 -0300

    fix(infrastructure): fixes .env.example and entrypoint.sh to run on docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,8 +39,7 @@ if [ "${UVICORN_RELOAD:-false}" = "true" ]; then
   --host 0.0.0.0 \
   --port 8000 \
   --reload \
-  --reload-dir /app \
-  --reload-use-polling
+  --reload-dir /app
 fi
 
 exec uvicorn app.main:create_app --factory --host 0.0.0.0 --port 8000
